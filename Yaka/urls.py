6c3fde3517bfe287d050ba330e05629ad765e97e
alvinkabwama <alkaleos10@gmail.com>
2019-04-09 20:35:31 +0300

    adding login screen

--- Yaka/urls.py
+++ Yaka/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('meter_data/', include('meter_data.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
